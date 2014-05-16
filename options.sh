e723b4e0f78c93c1a385fed8f223b3f8d855963f
Oleg Popadko <oleg.popadko@icloud.com>
2014-05-16 14:49:26 +0000

    Add apt-get libreadline-dev

--- options.sh
+++ options.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -qq -y --force-yes install gcc libpcre++-dev libcurl4-openssl-dev libxml2-dev libjpeg-dev libpng-dev libxpm-dev libfreetype6-dev libmcrypt-dev libmysql++-dev autoconf libevent-dev libltdl-dev libxml2-dev libxslt1-dev
+sudo apt-get -qq -y --force-yes install gcc libpcre++-dev libcurl4-openssl-dev libxml2-dev libjpeg-dev libpng-dev libxpm-dev libfreetype6-dev libmcrypt-dev libmysql++-dev autoconf libevent-dev libltdl-dev libxml2-dev libxslt1-dev libreadline-dev
 
 etxdir="$instdir/etc"
 
